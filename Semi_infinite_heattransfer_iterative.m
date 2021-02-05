cc1335c7be04ba73443a8485c61eda9bc2004d13
Henry <heve9547@colorado.edu>
2021-02-05 15:57:33 -0700

    Add files via upload

--- Semi_infinite_heattransfer_iterative.m
+++ Semi_infinite_heattransfer_iterative.m
@@ -3,8 +3,6 @@ clear, clc
 %% parameters
 T_a = readtable('Circleville2019.txt');
 TT = T_a(:,1:10);
-%T_air = TT{:,3}';
-T_air = f_to_c(90);%C
 T_Earth = TT{:,10}';
 TE_slist = T_Earth(1,2880:6552);% months 5-9 % months 4, april and october excluded
 TE_slist(141) = TE_slist(140); %141 is a NAN
@@ -19,20 +17,26 @@ k_PEH = .5; %W/m*K
 r1 = 0.1541;%m inner r
 r2 = 0.1683;%m outer r 
 A_tube =2*pi*r1; %m^2
-%t = 3600; %1hr in seconds
+h_wall = k_PEH/(r2-r1);%W/m^2*k 
 SA_tubes  = r2*2*pi*L_tubes;%total all tube length
 SA_tube = r2*2*pi*L_tube; %1 section of tube
 burial_D = 3.9624;% 13 feet
 %Air properties
-%https://www.engineeringtoolbox.com/convective-heat-transfer-d_430.html
+%https://en.wikipedia.org/wiki/Heat_transfer_coefficient#Internal_flow,_turbulent_flow
+%^^ SEE INTERNAL, TURBULENT FLOW & HEAT TRANSFER COEFFICIENT OF PIPE WALL
 V = 33454.7; %ft^3
 V = V*0.0283168; %m^3
 Cp = 1006; %J/kg*K
 rho = 1.225; %0.6601; %@6000 kg/m^3
-V_s = 0.1012799;% m^3/s 
-v = V_s/A_tube;
-hair = 10.45 - v + 10*v^(1/2);      
-%hair = 12; %turbulent air W/m*K
+CFM = (30.6+35.7)/2; %ft^3/min CFM per tube(averaged for the two subsystems)
+V_m3 = CFM*0.00047194745;% m^3/s %volumetric flow rate per tube 
+v_in = V_m3/A_tube; %m/s
+m_dot = V_m3*rho; %kg/s
+k_air = .02514;%@20C @ 1atm W/m*k 
+mu_air = 1.825*10^-5; %dynamic viscosity
+n = .33; %.4 for wall hotter than the bulk fluid, .33 for wall cooler than the bulk fluid
+h_air = .023*((m_dot*r1*2/mu_air)^.8)*(((mu_air*Cp)/k_air)^n)*(k_air/r1*2);
+h_pipe = 1/((1/h_air)+ (1/h_wall));
 %Dirt properties
 diff_soil = .91*10^-6; %m2/s
 k_soil = 2.1; %W/m*K 
@@ -43,34 +47,31 @@ T_Gw = f_to_c(60); %C
 T_Earthw = TE_w; %C earth temperature winter
 %Thermal resistances
 %R = L/k
-R_conv = (1/hair)/A_tube; %K/W
+R_conv = (1/h_air)/A_tube; %K/W
 R_pipe = (log(r2/r1))/(2*pi*k_PEH*L_tube);
 R_contact =1-(6/100);
 R_T = R_contact*(R_conv+R_pipe);
-
 %% EQ's  
 %METHOD - Row of pipes in semi-infinite solid 2.1.19
 %pg.75 in Conduction Heat Transfer Solutions
 s = 0.3048; %12in ->m
-Bi_1 = (hair*r1)/k_PEH;
+Bi_1 = (h_pipe*r1)/k_PEH;
 Bi_2 = (k_soil*burial_D*burial_D)/k_soil; % h=k*burialD
 D = burial_D/s;
-%SUMMER
+%SUMMER %NOT WORKING
 t2 = T_Earths;
 t1 = T_Gs;
 q = 2*pi*k_soil*(t2-t1)/((1/Bi_1)+log((burial_D/(pi*r1*D))*sinh(2*pi*(D+(D/Bi_2)))));
-T_delta3 = q *R_T*SA_tube*7; %7 or 8 is the number of tubes within each system
+T_delta3 = q *R_T*SA_tube*(1); %(7 + 8) is the number of tubes within each system
 T_EarthPS = T_Earths-T_delta3 %Earth Temp Post-Summer
 %WINTER
 t2w = T_EarthPS;
-t1w = T_Gw; %Was T_Gw, TE_w
+t1w = T_Gw; 
 q_w = 2*pi*k_soil*(t2w-t1w)/((1/Bi_1)+log((burial_D/(pi*r1*D))*sinh(2*pi*(D+(D/Bi_2)))));
 %Semi-infinite solid. Surface condition: constant surface heat flux. pg337
 %in Fundamental of Heat Transfer
-%t = 60;
 x = burial_D;
-%T_EarthPW = t2w-((2*q_w*((diff_soil*t/pi)^1/2)/k_soil)*exp((-x^2)/(4*diff_soil*t)))-(((q_w*x)/k_soil)*erfc(x/(2*sqrt(diff_soil*t))))
-t = 1:7.776e+6;
+t = 1:7.776e+6; %90 days in seconds (approximate length of winter)
 T_EarthPW = zeros(1,length(t));
 q_w = zeros(1,length(t));
 flag = 0;
@@ -81,9 +82,9 @@ for i=1:length(t)
     else
         T_EarthPW(i)=T_EarthPW(i-1)-((2*q_w(i-1)*((diff_soil*t(i)/pi)^1/2)/k_soil)*exp((-x^2)/(4*diff_soil*t(i))))-(((q_w(i)*x)/k_soil)*erfc(x/(2*sqrt(diff_soil*t(i)))));
         q_w(i) = 2*pi*k_soil*(T_EarthPW(i)-t1w)/((1/Bi_1)+log((burial_D/(pi*r1*D))*sinh(2*pi*(D+(D/Bi_2)))));
-        if  T_EarthPW(i) < t1w*1.1 && T_EarthPW(i) > t1w && flag==0
-            t_end = t(i)/(60*60);
-            disp([num2str(t_end),' Hours to reach ambient Earth temperature'])
+        if  T_EarthPW(i) < t1w*1.01 && T_EarthPW(i) > t1w && flag==0
+            t_end = t(i)/(60*60*24);
+            disp([num2str(t_end),' Days to reach ambient Earth temperature'])
             flag =1;
         end
     end
